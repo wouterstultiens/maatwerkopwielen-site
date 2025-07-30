80e82bacea3e4527ff8b7624d836b815183242c8
wouterstultiens <wouterstultiens@gmail.com>
2025-07-30 23:02:20 +0200

    tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#b91c1c',
+        dark: '#1e293b',
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
